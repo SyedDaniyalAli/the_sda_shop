f39dcdc9a84bd7a3453168f02b59119f8ce48f33
SyedDaniyalAli <SyedDaniyalAli2222@gmail.com>
2021-01-14 03:00:32 +0500

    Nested Models and Providers Video (195)

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -1,36 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/product.dart';
+import '../screens/product_detail_screen.dart';
 
 class ProductItem extends StatelessWidget {
-  final String id;
-  final String title;
-  final String imageUrl;
-
-  ProductItem({this.id, this.title, this.imageUrl});
+  // final String id;
+  // final String title;
+  // final String imageUrl;
+  //
+  // ProductItem({this.id, this.title, this.imageUrl});
 
   @override
   Widget build(BuildContext context) {
-    return ClipRRect(
-      borderRadius: BorderRadius.circular(10),
-      child: GridTile(
-        child: Image.network(
-          imageUrl,
-          fit: BoxFit.cover,
-        ),
-        footer: GridTileBar(
-          leading: IconButton(
-            icon: Icon(Icons.favorite),
-            color: Theme.of(context).accentColor,
-            onPressed: () {},
+    final product = Provider.of<Product>(context);
+    return GestureDetector(
+      onTap: () {
+        Navigator.of(context)
+            .pushNamed(ProductDetailScreen.routeName, arguments: product.id);
+      },
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(10),
+        child: GridTile(
+          child: Image.network(
+            product.imageUrl,
+            fit: BoxFit.cover,
           ),
-          title: Text(
-            title,
-            textAlign: TextAlign.center,
-          ),
-          backgroundColor: Colors.black87,
-          trailing: IconButton(
-            icon: Icon(Icons.shopping_cart),
-            color: Theme.of(context).accentColor,
-            onPressed: () {},
+          footer: GridTileBar(
+            leading: IconButton(
+              icon: Icon(
+                  product.isFavorite ? Icons.favorite : Icons.favorite_border),
+              color: Theme.of(context).accentColor,
+              onPressed: () {
+                product.toggleFavoriteStatus();
+              },
+            ),
+            title: Text(
+              product.title,
+              textAlign: TextAlign.center,
+            ),
+            backgroundColor: Colors.black87,
+            trailing: IconButton(
+              icon: Icon(Icons.shopping_cart),
+              color: Theme.of(context).accentColor,
+              onPressed: () {},
+            ),
           ),
         ),
       ),
---
+++ lib/widgets/products_grid.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/products_provider.dart';
+import '../widgets/product_item.dart';
+
+class ProductsGrid extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final productsData = Provider.of<Products>(context, listen: true).items;
+    return GridView.builder(
+      padding: EdgeInsets.all(10),
+      itemCount: productsData.length,
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10),
+      itemBuilder: (ctx, index) => ChangeNotifierProvider(
+        create: (ctx) => productsData[index],
+        child: ProductItem(
+          // id: productsData[index].id,
+          // title: productsData[index].title,
+          // imageUrl: productsData[index].imageUrl,
+        ),
+      ),
+    );
+  }
+}
