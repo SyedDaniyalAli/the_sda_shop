37ea6a3b7a77e9fb9f6e61884f10ed74a91bdb4d
SyedDaniyalAli <SyedDaniyalAli2222@gmail.com>
2021-01-30 17:11:28 +0500

    Using the "FutureBuilder" Widget & Improving the Code Video (254)

--- lib/screens/orders_screen.dart
+++ lib/screens/orders_screen.dart
@@ -13,34 +13,74 @@ class OrdersScreen extends StatefulWidget {
 }
 
 class _OrdersScreenState extends State<OrdersScreen> {
-  var _isLoading = false;
+  // var _isLoading = false;
+
+  // @override
+  // void initState() {
+  //   //Not an optimal way
+  //   // Future.delayed(Duration.zero).then((_) async{
+  //   //   setState(() {
+  //   //     _isLoading=true;
+  //   //   });
+  //   //   await Provider.of<Orders>(context, listen: false).fetchAndSetOrders();
+  //   //   setState(() {
+  //   //     _isLoading=false;
+  //   //   });
+  //   // });
+  //
+  //   //Optimal way by using (listen=false)
+  //   // _isLoading = true;
+  //   // Provider.of<Orders>(context, listen: false).fetchAndSetOrders().then((_) {
+  //   //   setState(() {
+  //   //     _isLoading = false;
+  //   //   });
+  //   // });
+  //
+  //   super.initState();
+  // }
+
+  //If You have multiple future then
+  Future _ordersFuture;
+
+  Future _obtainOrdersFuture() {
+    return Provider.of<Orders>(context, listen: false).fetchAndSetOrders();
+  }
 
   @override
   void initState() {
-    Future.delayed(Duration.zero).then((_) async{
-      setState(() {
-        _isLoading=true;
-      });
-      await Provider.of<Orders>(context, listen: false).fetchAndSetOrders();
-      setState(() {
-        _isLoading=false;
-      });
-    });
+    _ordersFuture = _obtainOrdersFuture();
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    final orderData = Provider.of<Orders>(context);
+    // final orderData = Provider.of<Orders>(context); committed because it re-build whole widget again and again
     return Scaffold(
       appBar: AppBar(
         title: Text('Your Orders'),
       ),
       drawer: AppDrawer(),
-      body: _isLoading ? Center(child: CircularProgressIndicator()): ListView.builder(
-        itemBuilder: (ctx, index) => OrderItem(orderData.orders[index]),
-        itemCount: orderData.orders.length,
-      ),
+      body:
+          //Best Way to fetch product using Future Builder
+          FutureBuilder(
+              future: _ordersFuture,
+              builder: (context, dataSnapshot) {
+                if (dataSnapshot.connectionState == ConnectionState.waiting) {
+                  return Center(child: CircularProgressIndicator());
+                }
+                if (dataSnapshot.error != null) {
+                  return Center(
+                    child: Text('An error occurred!'),
+                  );
+                } else {
+                  return Consumer<Orders>(
+                      builder: (context, orderData, child) => ListView.builder(
+                            itemCount: orderData.orders.length,
+                            itemBuilder: (ctx, index) =>
+                                OrderItem(orderData.orders[index]),
+                          ));
+                }
+              }),
     );
   }
 }
