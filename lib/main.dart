ee347c2cc71c5e882b24a37055e04342d4ecb32d
SyedDaniyalAli <SyedDaniyalAli2222@gmail.com>
2021-02-01 02:13:56 +0500

    Auth screen added Video (260)

--- lib/main.dart
+++ lib/main.dart
@@ -5,11 +5,12 @@ import './providers/cart.dart';
 import './providers/orders.dart';
 import './providers/products_provider.dart';
 import './screens/cart_screen.dart';
+import './screens/edit_product_screen.dart';
 import './screens/orders_screen.dart';
 import './screens/product_detail_screen.dart';
 import './screens/products_overview_screen.dart';
-import './screens/edit_product_screen.dart';
 import './screens/user_products_screen.dart';
+import 'screens/auth_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -30,12 +31,13 @@ class MyApp extends StatelessWidget {
             fontFamily: 'Lato'),
         // home: ProductsOverviewScreen(),
         routes: {
-          '/': (ctx) => ProductsOverviewScreen(),
+          '/': (ctx) => AuthScreen(),
           ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
           CartScreen.routeName: (ctx) => CartScreen(),
           OrdersScreen.routeName: (ctx) => OrdersScreen(),
           UserProductsScreen.routeName: (ctx) => UserProductsScreen(),
-          EditProductScreen.routeName: (ctx)=>EditProductScreen(),
+          EditProductScreen.routeName: (ctx) => EditProductScreen(),
+          ProductsOverviewScreen.routeName: (ctx) => ProductsOverviewScreen(),
         },
       ),
     );
--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -11,6 +11,7 @@ import '../widgets/products_grid.dart';
 enum FilterOptions { Favorites, All }
 
 class ProductsOverviewScreen extends StatefulWidget {
+  static const routeName = '/product_overview';
   @override
   _ProductsOverviewScreenState createState() => _ProductsOverviewScreenState();
 }
