6172f6f8fea8b2a9169778d7e9b4b7fe26a12503
Syed Daniyal Ali <SyedDaniyalAli2222@gmail.com>
2021-02-23 13:50:42 +0500

    281 Adding a "Hero" Transition

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -27,12 +27,17 @@ class ProductItem extends StatelessWidget {
       child: ClipRRect(
         borderRadius: BorderRadius.circular(10),
         child: GridTile(
-          child: FadeInImage(
-            placeholder: AssetImage("assets/images/product-placeholder.png"),
-            image: NetworkImage(
-              product.imageUrl,
+           
+          child: Hero(
+            //FadeIn Image Animation~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+            tag: product.id,
+            child: FadeInImage(
+              placeholder: AssetImage("assets/images/product-placeholder.png"),
+              image: NetworkImage(
+                product.imageUrl,
+              ),
+              fit: BoxFit.cover,
             ),
-            fit: BoxFit.cover,
           ),
           footer: GridTileBar(
             leading: IconButton(
